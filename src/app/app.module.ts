99aaff4ac6eee5bf967839c3526edfcf348db6fe
3amry <engahmedelamry@gmail.com>
2022-02-02 20:03:35 +0200

    add footer and products comps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
